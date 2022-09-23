d6ed96d6014d175345d5259ac3eec353d802b59e
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-09-22 21:37:34 -0300

    Adicionando usuario no core data e listando atributo nome do usuario salvo.

--- HandlingDatasWithCoreData/ViewController.swift
+++ HandlingDatasWithCoreData/ViewController.swift
@@ -16,8 +16,38 @@ class ViewController: UIViewController {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
-        let user = NSEntityDescription.insertNewObject(forEntityName: "User", into: context)
+        let getUserDatas = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        do {
+            let users = try context.fetch(getUserDatas)
+            
+            if users.count > 0 {
+                for user in users as! [NSManagedObject] {
+                    if let userName = user.value(forKey: "name") {
+                        print(userName)
+                    }
+                }
+                
+            } else {
+                print("Nenhum usuário encontrado.")
+            }
+            
+        } catch  {
+            print("Ocorreu um erro ao recuperar os dados.")
+        }
         
+//        let user = NSEntityDescription.insertNewObject(forEntityName: "User", into: context)
+//
+//        user.setValue("Joao Andrey", forKey: "name")
+//        user.setValue( 29 , forKey: "age")
+//        user.setValue("andreyantochevis", forKey: "login")
+//        user.setValue("3456", forKey: "password")
+//
+//        do {
+//            try context.save()
+//            print("Usuário salvo com sucesso!")
+//        } catch {
+//            print("Ocorreu um erro ao salvar.")
+//        }
         
     }
 
